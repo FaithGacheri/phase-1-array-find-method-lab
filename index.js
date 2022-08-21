acba5966bd767351f741912201086560eb82969f
“[firstname <faith.gacheri@student.moringaschool.com>
2022-08-21 20:45:28 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(games){
+   
+  let target = games.find((elem,index,arr)=>{
+    return elem.result === 'W'
+  })
+
+  if (target){
+    return target.year
+  }
+  return undefined
+}
